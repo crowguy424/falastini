30f4ddc9ff5980c7093e283335e5f4c28fb5d362
Crow Guy <37484491+crowguy424@users.noreply.github.com>
2024-09-19 23:33:49 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+let ZainabMaagi = [2, 3, 6, 8, 11];
+console.log(ZainabMaagi[0]);
+console.log(ZainabMaagi[1]);
+console.log(ZainabMaagi[3 - 1]);
+console.log(ZainabMaagi[3 + 1]);
+console.log(ZainabMaagi[2 + 1]);
+*/
+/*
+let maagi = "Tasneem";
+console.log(maagi.toLocaleUpperCase());
+let khanki = "FARIHA";
+console.log(khanki.toLocaleLowerCase());
+*/
+/*
+let sequencer = [1, 2, 3];
+sequencer.push(1);
+sequencer.push(7);
+sequencer.push(2);
+console.log(sequencer);
+*/
